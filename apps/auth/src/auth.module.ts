3682fd7b85540574e4ae5fcc13f8e120e3768296
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-11-01 06:53:52 +0100

    feat: setup MongoDB with Mongoose
    - Added MongooseModule to `auth.module.ts` with dynamic configuration using ConfigService.
    - Added MongooseModule to `app.module.ts` with dynamic configuration using ConfigService.
    - Configured Mongoose to connect to MongoDB using the `LOCAL_MONGODB_URI` environment variable.

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RmqModule } from '@app/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -12,6 +13,12 @@ import { ConfigModule } from '@nestjs/config';
       }
     ),
     RmqModule.register('auth'),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.getOrThrow('LOCAL_MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
--- apps/buddy/src/app.module.ts
+++ apps/buddy/src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -12,6 +13,12 @@ import { ConfigModule } from '@nestjs/config';
       }
     ),
     AuthModule,
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.getOrThrow('LOCAL_MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
