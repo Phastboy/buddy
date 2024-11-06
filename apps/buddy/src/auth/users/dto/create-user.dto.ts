a23cef91994cc70d76524141e67a445b49afde8f
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-11-07 00:29:15 +0100

    feat(auth): enhance user registration with validation in CreateUserDto

--- apps/buddy/src/auth/users/dto/create-user.dto.ts
+++ apps/buddy/src/auth/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  password: string;
+}
