e3375a3225a474971c381b490ef5ea9945040f7d
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-11-09 04:39:27 +0100

    refactor(auth): replace send method with emitEvent in AuthService
    - Removed send method and added emitEvent method for emitting events.
    - Added logging for event emission success and failure.

--- apps/buddy/src/auth/auth.service.ts
+++ apps/buddy/src/auth/auth.service.ts
@@ -1,17 +1,22 @@
-import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { lastValueFrom } from 'rxjs';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(@Inject('RABBITMQ_AUTH_CLIENT') private client: ClientProxy) {}
 
-  async send(pattern: string, data: any) {
-    return await lastValueFrom(this.client.send(pattern, data), {
-        defaultValue: {
-            status: 'error',
-            message: 'No response from auth microservice'
-        }
+  async emitEvent(pattern: any, data: any) {
+    return new Promise((resolve, reject) => {
+      this.client.emit(pattern, data).subscribe({
+        complete: () => {
+          Logger.log(`Event emitted for pattern: ${pattern}`, 'AuthService');
+          resolve(true);
+        },
+        error: (err) => {
+          Logger.error(`Failed to emit event for pattern: ${pattern}. Error: ${err.message}`, err.stack, 'AuthService');
+          reject(err);
+        },
+      });
     });
   }
 }
