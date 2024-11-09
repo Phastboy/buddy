d1cdba8e845d18ae715763611d910725204d90b5
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-11-09 17:39:43 +0100

    fix(users): handle duplicate key error and improve logging in UsersService
    - Added ConflictException for handling MongoDB duplicate key errors.
    - Improved logging for user creation and error scenarios in UsersService.

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, ConflictException } from '@nestjs/common';
 import { User, UserDocument } from './users.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -6,17 +6,22 @@ import { CreateUserDto } from './dto/register.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
-    
-    async create(user: CreateUserDto): Promise<UserDocument> {
-      Logger.log(`Creating user: ${user.username}`, 'UsersService');
-        try {
-          return await this.userModel.create(user);
-        } catch (error: any) {
-          if (error) {
-            Logger.error(`Failed to create user: ${user.username}. Error: ${error.message}`, error.stack, 'UsersService');
-            throw new Error(`Failed to create user: ${user.username}. Error: ${error.message}`);
-          }
-        }
-      }      
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(user: CreateUserDto): Promise<UserDocument> {
+    Logger.log(`Creating user: ${user.username}`, 'UsersService');
+    try {
+      return await this.userModel.create(user);
+    } catch (error: any) {
+      if (error.code === 11000) { // MongoDB duplicate key error
+        const duplicateKey = Object.keys(error.keyPattern)[0];
+        console.log(duplicateKey);
+        Logger.warn(`Duplicate key error on: ${duplicateKey}`, 'UsersService');
+        throw new ConflictException(`A user with this ${duplicateKey} already exists.`);
+      } else {
+        Logger.error(`Failed to create user: ${user.username}. Error: ${error.message}`, error.stack, 'UsersService');
+        throw new Error(`Failed to create user: ${user.username}. Error: ${error.message}`);
+      }
+    }
+  }
 }
