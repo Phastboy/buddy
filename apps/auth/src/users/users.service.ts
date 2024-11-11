16cfc08cd434d3d62bd6d64d11c425f3ed69e1ba
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-11-11 02:59:55 +0100

    **refactor(users): standardize error handling in create method**
    Refactored the `create` method in `UsersService` to standardize error handling. Removed the custom response object and replaced it with NestJS exceptions (`BadRequestException` and `InternalServerErrorException`) for better consistency and clarity.

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import { User, UserDocument } from './users.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -8,35 +8,18 @@ import { CreateUserDto } from './dto/register.dto';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(
-    user: CreateUserDto,
-  ): Promise<{ success: boolean; data?: UserDocument; message?: string }> {
+  async create(user: CreateUserDto): Promise<UserDocument> {
     Logger.log(`Creating user: ${user.username}`, 'UsersService');
     try {
-      const createdUser = await this.userModel.create(user);
-      return {
-        success: true,
-        data: createdUser,
-      };
+      return await this.userModel.create(user);
     } catch (error: any) {
       if (error.code === 11000) {
-        // MongoDB duplicate key error
         const duplicateKey = Object.keys(error.keyPattern)[0];
         Logger.warn(`Duplicate key error on: ${duplicateKey}`, 'UsersService');
-        return {
-          success: false,
-          message: `A user with this ${duplicateKey} already exists.`,
-        };
+        throw new BadRequestException(`A user with this ${duplicateKey} already exists.`);
       } else {
-        Logger.error(
-          `Failed to create user: ${user.username}. Error: ${error.message}`,
-          error.stack,
-          'UsersService',
-        );
-        return {
-          success: false,
-          message: `Failed to create user: ${user.username}. Error: ${error.message}`,
-        };
+        Logger.error(`Failed to create user: ${user.username}. Error: ${error.message}`, error.stack, 'UsersService');
+        throw new InternalServerErrorException(`Failed to create user: ${user.username}. Error: ${error.message}`);
       }
     }
   }
