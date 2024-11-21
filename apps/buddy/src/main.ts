35f79df9807c71891cde8dbc6dcf8092d4e9e3d1
Hammed Anuoluwapo Pelumi <Hammed Anuoluwapo Pelumi>
2024-11-21 07:49:22 -0800

    refactor: remove RabbitMQ microservice connection from main.ts
    Removed the unneccessary RabbitMQ microservice connection setup from main.ts to streamline the application bootstrap process.

--- apps/buddy/src/main.ts
+++ apps/buddy/src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
@@ -12,18 +11,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ResponseInterceptor)
 
-  // Connect to RabbitMQ
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://localhost:5672'],
-      queue: 'buddy',
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
   const config = new DocumentBuilder()
     .setTitle('Buddy')
     .setDescription('Buddy API documentation')
@@ -33,7 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(`Buddy app is listening at ${port}`, 'Buddy');
 }
