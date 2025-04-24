15b8b2e9b33bac026e7d0f6c45d6417e027f821f
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-24 16:17:58 +0100

    feat: restructure app layout; remove unused screens and implement new tab navigation

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,13 +1,17 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { Text, View } from '@/components/Themed';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
+        <Text style={styles.title}>This screen doesn't exist.</Text>
+
         <Link href="/" style={styles.link}>
-          Go to home screen!
+          <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
       </View>
     </>
@@ -21,8 +25,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
   link: {
     marginTop: 15,
     paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
   },
 });
