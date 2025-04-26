c956c11d174888e63b5c472f9832368fedde570f
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-26 16:34:23 +0100

    feat: Update ScrollAwareHeaderProps to require height and fadeDistance parameters

--- components/headers/scrollAwareHeader/type.ts
+++ components/headers/scrollAwareHeader/type.ts
@@ -7,8 +7,8 @@ export interface ScrollAwareHeaderProps {
   children: React.ReactNode;
   scrollY: SharedValue<number>;
   isScrollingUp: SharedValue<boolean>;
-  height?: number;
-  fadeDistance?: number;
+  height: number;
+  fadeDistance: number;
   containerStyle?: StyleProps;
   showBackButton?: boolean;
   childrenContainerStyle?: StyleProps;
