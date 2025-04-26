020aad1dfe4c3deb31033f08646fe5433a32d8e3
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-26 04:28:14 +0100

    feat: Enhance UserAvatar component to accept custom styles for improved flexibility

--- components/headers/userAvatar.tsx
+++ components/headers/userAvatar.tsx
@@ -1,21 +1,30 @@
 import { Ionicons } from '@expo/vector-icons';
-import { Image } from 'react-native';
+import { Image, ImageStyle } from 'react-native';
 
 const DEFAULT_AVATAR_URL =
   'https://avatars.githubusercontent.com/u/75147299?v=4';
 
-export default function UserAvatar({ avatarUrl }: { avatarUrl?: string }) {
+export default function UserAvatar({
+  avatarUrl,
+  style,
+}: {
+  avatarUrl?: string;
+  style?: ImageStyle;
+}) {
   if (!avatarUrl) {
     return <Ionicons name="person-circle-outline" size={40} color="gray" />;
   }
   return (
     <Image
       source={{ uri: avatarUrl }}
-      style={{
-        width: 40,
-        height: 40,
-        borderRadius: 20,
-      }}
+      style={[
+        {
+          width: 40,
+          height: 40,
+          borderRadius: 20,
+        },
+        style,
+      ]}
     />
   );
 }
